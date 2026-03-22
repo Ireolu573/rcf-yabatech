47b5163182e433677296750004940a5d4383dded
Ireolu573 <stepanovaire@gmail.com>
2026-03-22 17:40:04 +0100

    simplify supabase client

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -4,11 +4,4 @@ import type { Database } from './types';
 const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
 const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_ANON_KEY, {
-  auth: {
-    persistSession: true,
-    autoRefreshToken: true,
-    detectSessionInUrl: true,
-    storage: window.sessionStorage,
-  }
-});
+export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_ANON_KEY);
